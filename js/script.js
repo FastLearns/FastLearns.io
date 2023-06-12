dfbc4f49ab3d649e3495273b7f9290867fdc42c8
FastLearns <agustinpenagosl2606@gmail.com>
2023-06-12 10:55:48 -0500

    Añadido el modo oscuro con localStorage

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ darkModeIcon.onclick = () => {
     darkModeIcon.classList.toggle('bx-sun');
     document.body.classList.toggle('darkMode');
     
+    if(document.body.classList.contains('darkMode')){
+        localStorage.setItem('dark-mode', 'true');        
+    } else{
+        localStorage.setItem('dark-mode', 'false');
+    }
 };
+
+if(localStorage.getItem('dark-mode') === 'true'){
+    document.body.classList.add('darkMode');
+    darkModeIcon.classList.add('bx-sun');
+} else{
+    document.body.classList.remove('darkMode');
+    darkModeIcon.classList.remove('bx-sun');
+}
